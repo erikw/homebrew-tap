6fbe86cee50ee80aeff871ecfcd3468b1c76d124
Erik Westrup <erik.westrup@gmail.com>
2022-02-11 22:46:24 +0100

    Added: Formula/restic-automatic-backup-scheduler.rb

---
+++ Formula/restic-automatic-backup-scheduler.rb
@@ -0,0 +1,45 @@
+class ResticAutomaticBackupScheduler < Formula
+  desc "Automatic restic backup schedule using Backblaze B2 storage & macOS LaunchAgents"
+  homepage "https://github.com/erikw/restic-systemd-automatic-backup"
+  url "https://github.com/erikw/restic-systemd-automatic-backup/archive/refs/tags/v5.2.0.tar.gz"
+  sha256 "15f8a553e29ccb6c65ff2044392985834c451c51b4405891a47698442f0a532c"
+  license "BSD-3-Clause"
+
+  depends_on "restic"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "INSTALL_PREFIX=/usr/local", "SYSCONFDIR=#{etc}/..", "install-launchagent"
+  end
+
+  def caveats
+    <<~EOS
+      ===========
+      To get started with backups, do the following:
+
+      1. Edit config files at /etc/restic/*.env.sh
+      2. Possibly edit schedule and profile to use in
+         ~/Library/LaunchAgents/com.github.erikw.restic-automatic-backup.plist
+      3. Install, enable and start the first run!
+         $ launchctl bootstrap gui/$UID ~/Library/LaunchAgents/com.github.erikw.restic-automatic-backup.plist
+         $ launchctl enable gui/$UID/com.github.erikw.restic-automatic-backup
+         $ launchctl kickstart -p gui/$UID/com.github.erikw.restic-automatic-backup
+      4. Use the disable command to temporarily pause the agent, or bootout to uninstall it.
+         $ launchctl disable gui/$UID/com.github.erikw.restic-automatic-backup
+         $ launchctl bootout gui/$UID/com.github.erikw.restic-automatic-backup
+
+
+      **NOTE** If you updated the .plist file, you need to issue the bootout followed by bootrstrap
+      and enable sub-commands of launchctl. This will guarantee that the file is properly reloaded.
+
+
+      See full tutorial at https://github.com/erikw/restic-systemd-automatic-backup
+      ===========
+    EOS
+  end
+
+  test do
+    act = shell_output("resticw -h").lines.first.strip
+    exp = "A little wrapper over restic just to handle profiles and environment loading."
+    assert_equal exp, act
+  end
+end
