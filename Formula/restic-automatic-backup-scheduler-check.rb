37c9b55049a93ebc08ad9a25b73f0f6517225431
Erik Westrup <erik.westrup@gmail.com>
2022-02-15 13:20:11 +0100

    restic-automatic-backup-scheduler-check 7.2.0 (new formula)
    Add restic-check formula
    
    Closes #12.
    
    Signed-off-by: Erik Westrup <erik.westrup@gmail.com>

---
+++ Formula/restic-automatic-backup-scheduler-check.rb
@@ -0,0 +1,34 @@
+# This formula is not intended to be installed on its own, just as a dependency for restic-automatic-backup-scheduler
+class ResticAutomaticBackupSchedulerCheck < Formula
+  desc "Install restic-check LaunchAgent for restic-automatic-backup-scheduler formula"
+  homepage "https://github.com/erikw/restic-automatic-backup-scheduler"
+  url "https://github.com/erikw/restic-automatic-backup-scheduler/archive/refs/tags/v7.2.0.tar.gz"
+  sha256 "0db7f747067427ae8ec5ad01d7d407588965d5259354401d93adda55c3e2ce79"
+  license "BSD-3-Clause"
+
+  depends_on "bash"
+
+  def install
+    args = %W[
+      PREFIX=#{prefix}
+      INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      SYSCONFDIR=#{etc}/..
+      LAUNCHAGENTDIR=#{prefix}
+    ]
+
+    # Build and install only the LaunchAgent.
+    # Don't use string interpolation to avoid fault brew-audit check.
+    # "#{prefix}/Lib" should be "#{lib}"
+    plist = prefix.to_s + "/Library/LaunchAgents/com.github.erikw.restic-check.plist"
+    system "make", plist, *args
+
+    # The LaunchAgent need to have a special name for brew-services to pick it up.
+    # Reference: https://docs.brew.sh/Formula-Cookbook#launchd-plist-files
+    prefix.install_symlink \
+      "Library/LaunchAgents/com.github.erikw.restic-check.plist" => "#{plist_name}.plist"
+  end
+
+  test do
+    system "true"
+  end
+end
