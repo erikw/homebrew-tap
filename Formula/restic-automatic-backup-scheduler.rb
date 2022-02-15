5d47b526976e0420c8c43a53cd00ce2d087f57e2
Erik Westrup <erik.westrup@gmail.com>
2022-02-15 17:32:13 +0000

    restic-automatic-backup-scheduler: update 7.3.0 bottle.

--- Formula/restic-automatic-backup-scheduler.rb
+++ Formula/restic-automatic-backup-scheduler.rb
@@ -6,9 +6,9 @@ class ResticAutomaticBackupScheduler < Formula
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/erikw/homebrew-tap/releases/download/restic-automatic-backup-scheduler-7.2.0_1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "084846da53e6d634d8a86ac793ee435d480a0ae11faeadde4ffce2f74c00e898"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "af96fbefc3c39f5712d79663ef773f50375ff15724b286427062fa96fabd5ec6"
+    root_url "https://github.com/erikw/homebrew-tap/releases/download/restic-automatic-backup-scheduler-7.3.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "88ac5b436c22415efde7c27dde92b28d1e73349d70bae1f2f7f02ef541cda363"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "80a641a32d9495a0a160b9af59c41653123c33f77e6f23e6379a67c6405f1b81"
   end
 
   depends_on "bash"
