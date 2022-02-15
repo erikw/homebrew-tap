a8f2d65bb1057a5ccd131acdc904948d29896f3b
Erik Westrup <erik.westrup@gmail.com>
2022-02-15 12:26:57 +0000

    restic-automatic-backup-scheduler-check: add 7.2.0 bottle.

--- Formula/restic-automatic-backup-scheduler-check.rb
+++ Formula/restic-automatic-backup-scheduler-check.rb
@@ -6,6 +6,12 @@ class ResticAutomaticBackupSchedulerCheck < Formula
   sha256 "0db7f747067427ae8ec5ad01d7d407588965d5259354401d93adda55c3e2ce79"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/erikw/homebrew-tap/releases/download/restic-automatic-backup-scheduler-check-7.2.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "992be52fd325fb096fc19b7bb5be8c54d8f956f60bfa4a2afc7c2eba0e32aa4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "13c4025fbd23c67e8e666a4d5fa06f096975c50fc6a2936bd1c877b1a5626586"
+  end
+
   depends_on "bash"
 
   def install
